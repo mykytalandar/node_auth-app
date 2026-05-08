57e9748747b375ad93e031f09e052cf32f96c1cd
Mykyta Landar <mykytalandar@gmail.com>
2026-05-08 02:09:02 +0200

    moved the .env file to the root of the project

--- setup.js
+++ setup.js
@@ -1,6 +1,5 @@
-import dotenv from 'dotenv';
+import dotenv from 'dotenv/config';
 import { client } from './src/backend/src/utils/db.js';
 
-dotenv.config({ path: './src/backend/.env' });
 
 client.sync({ force: true });
